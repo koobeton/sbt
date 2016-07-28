2200cc50b658ecb4a12b51d4cdabfe69e5fa3be7
Baburin Sergey <sbasba777@hotmail.com>
2016-07-28 07:30:47 +0300

    Lesson 3. Task 2056

---
+++ src/main/java/lesson1/task2056/Solution.java
@@ -0,0 +1,43 @@
+package lesson1.task2056;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> words = new ArrayList<>();
+        while (scanner.hasNext()) {
+            words.add(scanner.next());
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word.toLowerCase())) {
+                map.put(word.toLowerCase(), map.get(word.toLowerCase()) + 1);
+            } else {
+                map.put(word.toLowerCase(), 1);
+            }
+        }
+
+        int max = 0;
+        Set<String> maxWords = new TreeSet<>();
+
+        for (String word : map.keySet()) {
+            if (map.get(word) == max) {
+                maxWords.add(word);
+            } else if (map.get(word) > max) {
+                max = map.get(word);
+                maxWords.clear();
+                maxWords.add(word);
+            }
+        }
+
+        for (String word : maxWords) {
+            System.out.println(word);
+        }
+    }
+}
