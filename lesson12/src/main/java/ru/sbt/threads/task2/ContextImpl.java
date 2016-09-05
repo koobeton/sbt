603403032ba41f36fcc3196154f62bc023c07e27
Baburin Sergey <sbasba777@hotmail.com>
2016-09-05 14:59:04 +0300

    Lesson 14. Lesson 12. ExecutionManager. Concurrent

--- lesson12/src/main/java/ru/sbt/threads/task2/ContextImpl.java
+++ lesson12/src/main/java/ru/sbt/threads/task2/ContextImpl.java
@@ -1,6 +1,8 @@
 package ru.sbt.threads.task2;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ContextImpl implements Context {
 
@@ -9,6 +11,7 @@ public class ContextImpl implements Context {
     private final AtomicInteger completed;
     private final AtomicInteger failed;
     private final AtomicInteger interrupted;
+    private final Lock lock;
 
     public ContextImpl(ThreadPool pool, int totalTaskCount) {
         this.pool = pool;
@@ -16,6 +19,7 @@ public class ContextImpl implements Context {
         completed = new AtomicInteger();
         failed = new AtomicInteger();
         interrupted = new AtomicInteger();
+        lock = new ReentrantLock();
     }
 
     @Override
@@ -40,8 +44,11 @@ public class ContextImpl implements Context {
 
     @Override
     public boolean isFinished() {
-        synchronized (this) {
+        lock.lock();
+        try {
             return completed.get() + failed.get() + interrupted.get() == totalTaskCount;
+        } finally {
+            lock.unlock();
         }
     }
 
