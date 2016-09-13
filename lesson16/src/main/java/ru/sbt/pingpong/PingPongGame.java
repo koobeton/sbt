9a1ffe2579029ff3b1ac179afc0d95895f9708cb
Baburin Sergey <sbasba777@hotmail.com>
2016-09-13 09:09:17 +0300

    Lesson 16. PingPongGame

---
+++ lesson16/src/main/java/ru/sbt/pingpong/PingPongGame.java
@@ -0,0 +1,38 @@
+package ru.sbt.pingpong;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PingPongGame {
+
+    private static final Lock OUTER_LOCK = new ReentrantLock();
+    private static final Lock INNER_LOCK = new ReentrantLock();
+
+    public static void main(String... args) {
+
+        PingPongGame game = new PingPongGame();
+
+        new Thread(game::startGame, "Ping").start();
+        new Thread(game::startGame, "Pong").start();
+    }
+
+    private void startGame() {
+        while (true) {
+            OUTER_LOCK.lock();
+            INNER_LOCK.lock();
+            OUTER_LOCK.unlock();
+            makeTurn();
+            INNER_LOCK.unlock();
+        }
+    }
+
+    private void makeTurn() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Turn of " + Thread.currentThread().getName() +
+                    " was interrupted: " + e.getMessage(), e);
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
